00f99bbac37c38f2eba8ae51923c0a80eb717be9
Kinza Aftab <160029911+KinzaAft@users.noreply.github.com>
2025-02-09 13:22:45 +0500

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,9 @@
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
-import { defineCliConfig } from 'sanity/cli'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+console.log(`Sanity Project: ${projectId}, Dataset: ${dataset}`);
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export const config = {
+  projectId,
+  dataset,
+};
